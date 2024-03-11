850c32083d36a8deec5215bd4bdd8a235a226945
Tiberiu Badea <tcristian.b@gmail.com>
2024-03-11 14:01:58 +0000

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Main } from "./components/Main";
 import { Work } from "./components/Work";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+
 function App() {
   return (
     <div>
